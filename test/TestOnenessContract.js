f7f5ab515d1a6615ff2ecdef1a8f1226c956e092
Jay <>
2024-03-25 16:22:47 +0800

    update addresses on testnet

--- test/TestOnenessContract.js
+++ test/TestOnenessContract.js
@@ -12,11 +12,11 @@ describe("oneness", function () {
   let owner;
   let addrArray;
 
-  const usdcAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
-  const pairAddress = "0x0CADB198a6aa31f9C4A5ABA1a148E1051D5C9a53";
-  const routerAddress = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e";
+  const usdcAddress = "0x9BF054279893C80C84cd8C74C3be299BaB9C8a29";
+  const pairAddress = "0x1bB36FD31bF42b9444f374FE621a2949b4c5e75b";
+  const routerAddress = "0xb128A49f1382c942A5Ca3BDa7227cC464D2B08E0";
 
-let proxyAddress = "";
+let proxyAddress = "0x1e0185888A29Dea2a8e34Cd073b2B6Bd25b45e95";
 let usdcToken;
 
 let pairContract;
@@ -64,7 +64,8 @@ let pairContract;
     usdcAmount = ethers.utils.parseEther("100").toBigInt();
     console.log(usdcAmount)
 
-    await usdcToken.approve(proxy.address,ethers.utils.parseEther("1000000000000000"))
+    const tx=await usdcToken.approve(proxy.address,ethers.utils.parseEther("1000000000000000"))
+    await tx.wait();
     allowanceVal = await usdcToken.allowance(owner.address,proxy.address)
     console.info("allowance val:",allowanceVal);
     await expect(proxy.addLiquidity(usdcAmount))
